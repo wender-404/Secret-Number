188b3b9751e45258d4901e3f95648de5e2f0835e
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-01-12 18:09:20 -0300

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,13 @@
 import os
-print ("~$ Press Enter For Continue")
+def quebra():
+   print()
+   print()
+   print()
+os.system('clear')
+print ("[~$] Press Enter For Continue")
+quebra()
 a = input("> ")
+os.system('clear')
 os.system('apt install ruby')
 os.system('chmod +x start.rb')
 os.system('ruby start.rb')
