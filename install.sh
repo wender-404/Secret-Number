4ff12d36dde9656b4c60e3d6563ec93273912373
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-07-02 13:53:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
 
-if [ -f start.rb ]; then
-    apt install ruby -y
-    if [ $? -eq 0 ]; then
-        if [ -x start.rb ]; then
-            ruby start.rb
-        else
-            chmod a+x start.rb
-            ruby start.rb
-        fi
-    else
-         echo -e "[\e[31mX\e[0m] ERRO CONECTION NOT FOUND!"
-    fi
+clear
+cd $PREFIX/bin # caminho onde ficam os pacotes instalados
+
+if test -x ruby; then
+    echo -e "\e[32mThe ruby ​​package is installed\n starting the game..."; sleep 3
+    cd ~/Secret-Number; ruby game.rb
 else
-    echo -e "[\e[31mX\e[0m] Erro \e[32mstart.rb\e[0m NOT FOUND!"
+    echo -e "\e[1;31m The ruby ​​package is not installed\e[0m\n[...] \e[32minstalling..."
+    apt-get install ruby -y > /dev/null
+    if test $? -eq 0; then
+        echo -e "\e[32m The ruby ​​package has been installed\e[0m\starting the game..."; sleep 3
+        cd ~/Secret-Number; ruby game.rb
+    
+    else
+        clear; cd ~/Secret-Number
+        echo -e "\e[1;31m it was not possible to install ruby\e[0m\nrum \e[33mpkg install ruby -y\e[0m and rejoin the installation file again"
+     fi
 fi
-
-# Se kibar e gay :)
