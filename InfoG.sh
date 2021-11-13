9b039c6c0b8794ad51abe120c22d46eb51b7c68d
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-11-13 19:14:51 -0300

    Create InfoG.sh

---
+++ InfoG.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Autor     : Dreifus-404 <github.com/Dreifus-404
+# Versão    : 0.1
+# Descrição : Introdução ao mini game @ Secret-Number
+
+whiptail --title "@ Secret-Number [ Info. ]" --tailbox __modules__/infog.txt 13 90
