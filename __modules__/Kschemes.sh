e002773f09669be046fb82c391c82f5a5fcc711d
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-11-14 12:41:06 -0300

    Update Kschemes.sh

--- __modules__/Kschemes.sh
+++ __modules__/Kschemes.sh
@@ -6,11 +6,28 @@
 
 cd ~/
 
+Info_Kschemes=( "\n\n Kali Shemes 4 Termux, é uma ferramenta que disponibiliza"
+                "4 temas do Kali Linux OS, para o termux, a ferramenta Secret-Number"
+                "foi feita para ser executada a partir do tema 02 ao 04 para uma"
+                "melhor experiencia visual\n\n"
+                "[Manual]\n"
+                " Após o codigo do Kali Shemes 4 Termux ser aberto ele pede o tema"
+                "que deseja, basta escolher, após escolher ele pede o nome do usuario (username)"
+                "depois de declarado o nome de usuario, ele pede o nome da maquina\n"
+                "ex:\n"
+                "E.g. kali, android, redmi-6-pro, etc.\n>>> Termux\n\n"
+                " Caso nada seja colocado no nome de usuario, ele utiliza o [$(whoami)]\n"
+                " Caso nada seja colocado no nome da maquina, ele utiliza o [localhost]\n\n"
+                "<Pressione enter para continuar>\n" )
+
+msg_Kschemes=`echo -e "${Info_Kschemes[*]}"`
+
 if [ -x kalischemes4termux ]; then
     echo -e " \e[1;32m[+] Kalischemes4termux isn't installed!\e[0m"
     echo -e " \e[1;33m[•] Loading program..."
 
     sleep 3
+    whiptail --title "Kali Schemes 4 Termux" --msgbox "$msg_Kschemes" 23 90
 
     cd ~/kalischemes4termux
     ./kalischemes4termux.sh
@@ -22,7 +39,8 @@ else
         echo -e " \e[1;32m[✓] KaliSchemes4Termux isn't installed!\e[0m"
         echo -e " \e[1;33m[•] Loading program..."
 
-        sleep 1
+        sleep 3
+        whiptail --title "Kali Schemes 4 Termux" --msgbox "$msg_Kschemes" 23 90
 
         cd ~/kalischemes4termux
         ./kalischemes4termux
